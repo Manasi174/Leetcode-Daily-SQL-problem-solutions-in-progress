f8510f0672b92d2c7e012dd6f8455ce9a14fed53
Manasi <manasi174@gmail.com>
2026-09-04 00:03:40 -0700

    1731 The number of employees who report to each employee easy

---
+++ 1731--The number of employees who report to each employee.sql
@@ -0,0 +1,13 @@
+--1731 The number of employees who report to each employee
+-- https://leetcode.com/problems/The-number-of-employees-who-report-to-each-employee/
+
+SELECT 
+    e1.employee_id,
+    e1.name,
+    COUNT(e2.employee_id) AS reports_count,
+    ROUND(AVG(CAST(e2.age AS FLOAT)), 0) AS average_age
+FROM Employees e1
+INNER JOIN Employees e2
+    ON e1.employee_id = e2.reports_to
+GROUP BY e1.employee_id, e1.name
+ORDER BY e1.employee_id
